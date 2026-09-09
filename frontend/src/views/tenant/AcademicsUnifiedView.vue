fc71b04cb4723315c25f9354bd46e561c16057d4
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-09 22:01:01 +0300

    Make academics the single unified view

--- frontend/src/views/tenant/AcademicsUnifiedView.vue
+++ frontend/src/views/tenant/AcademicsUnifiedView.vue
@@ -1,11 +1,7 @@
 <script setup>
 import AcademicsView from './AcademicsViewFixed.vue'
-import TeacherSubjectsPanel from './TeacherSubjectsPanel.vue'
 </script>
 
 <template>
-  <div class="container-fluid pt-4">
-    <TeacherSubjectsPanel />
-  </div>
   <AcademicsView />
 </template>
