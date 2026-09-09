54d63b506afc61315c5f1447470270d7954315d1
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-09 21:48:52 +0300

    Unify teacher subjects with academics

---
+++ frontend/src/views/tenant/AcademicsUnifiedView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AcademicsView from './AcademicsViewFixed.vue'
+import TeacherSubjectsPanel from './TeacherSubjectsPanel.vue'
+</script>
+
+<template>
+  <div class="container-fluid pt-4">
+    <TeacherSubjectsPanel />
+  </div>
+  <AcademicsView />
+</template>
